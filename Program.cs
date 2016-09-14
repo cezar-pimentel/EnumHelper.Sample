39b641357695e4657b98773734c8f8c1b8a4467d
CEZAR PIMENTEL <cezar.pimentel@gmail.com>
2016-09-14 13:46:54 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+
+namespace EnumHelper.Sample
+{
+	internal class Program
+	{
+		internal enum TestEnum
+		{
+			[Description("ITEM 1 DESCRIPTION")]
+			Item1
+		}
+
+		private static void Main()
+		{
+			Console.WriteLine($"{EnumHelper<TestEnum>.GetDescription(TestEnum.Item1)}");
+			Console.WriteLine("\nFINISHED");
+			Console.ReadLine();
+		}
+	}
+
+	internal static class EnumHelper<T> where T : struct
+	{
+		internal static string GetDescription(T value)
+		{
+			var field = typeof(T).GetField(value.ToString());
+			return field.GetCustomAttributes(typeof(DescriptionAttribute), false).Cast<DescriptionAttribute>().Select(x => x.Description).FirstOrDefault();
+		}
+	}
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EnumHelper.Sample")]
+[assembly: AssemblyDescription("EnumHelper Sample")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MEPSI")]
+[assembly: AssemblyProduct("EnumHelper.Sample")]
+[assembly: AssemblyCopyright("Copyright © CEZAR PIMENTEL 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("0404BF22-97E9-4385-95FE-11E39AFABD7B")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
